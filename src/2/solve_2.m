e8c8a3ffa0ccabb6ef63b5f0fd3d04e5fd8a66c2
Arash Rouhani <miffoljud@gmail.com>
2012-10-18 00:24:10 -0400

    Table works now

--- src/2/solve_2.m
+++ src/2/solve_2.m
@@ -1,6 +1,7 @@
 sigmas = arrayfun(@(x) 2^-x,1:n);
 rng(0); % So program becomes deterministic
 A = rand_sing(sigmas);
+qr_methods = {'classi', 'stable', 'househ'}
 
 [Q_classi, R_classi] = gram_classi(A);
 [Q_stable, R_stable] = gram_stable(A);
@@ -8,6 +9,13 @@ A = rand_sing(sigmas);
 [R_househ, V_househ] = househ(A);
 Q_househ = V_gives_Q(V_househ);
 
+for m = qr_methods
+  m=m{1}
+  eval(['norm_Q_' m ' = ' 'norm(Q_' m ')'])
+  path = ['../../data/' 'norm-q-' m '-' num2str(n) '.dat']
+  norm_var = ['norm_Q_' m]
+  system(['echo ' num2str(eval(norm_var)) ' > ' path])
+end
 norm_Q_classi = norm(Q_classi, 2)
 norm_Q_stable = norm(Q_stable, 2)
 norm_Q_househ = norm(Q_househ, 2)
