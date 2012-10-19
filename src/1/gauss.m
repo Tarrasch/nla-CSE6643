71e73de04cf7e6c45f27d828793f78e0d6ced0b2
Arash Rouhani <miffoljud@gmail.com>
2012-10-19 02:39:01 -0400

    Worked

--- src/1/gauss.m
+++ src/1/gauss.m
@@ -7,12 +7,16 @@ U = A; L = eye(m); P = eye(m);
 for k = 1:m-1
   [ignore, i] = max(abs(U(k:m,k)));
   i = i + k - 1;
-  U([k, i], k:m) = U([i, k], k:m);
+  U1=U(k, k:m);
+  U2=U(i, k:m);
+  U(i, k:m) = U1;
+  U(k, k:m) = U2;
+  maxx=U2(1);
   L([k, i], 1:k-1) = L([i, k], 1:k-1);
   P([k, i], :) = P([i, k], :);
   for j = k+1:m
-    L(j, k) = U(j, k)/U(k, k);
-    U(j, k:m) = U(j, k:m) - L(j, k)*U(k, k:m);
+    L(j, k) = U(j, k)/maxx;
+    U(j, k:m) = U(j, k:m) - L(j, k)*U2;
   end
 end
 
--- src/1/go.m
+++ src/1/go.m
@@ -1,6 +1,8 @@
 hold on
 fplot(@(x) -(x*x*x/6 - x*x/4 + x/12), [0 1], 'r')
 
-for n = [50 100 200 400 800]
+for n = [50 100 200 400 800 1600 3200]
+  tic
   solve
+  toc
 end
