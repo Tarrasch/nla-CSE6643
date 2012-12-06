01ca950281fe28b2032bb4f4ad686c97eaaef19d
Arash Rouhani <miffoljud@gmail.com>
2012-12-06 12:33:40 -0500

    Make rq_iteration output the number of iterations

--- src/1/rq_iteration.m
+++ src/1/rq_iteration.m
@@ -1,17 +1,17 @@
-function [ v_old ] = rq_iteration( A, v_guess )
+function [ v_old, iterations ] = rq_iteration( A, v_guess )
 %RQ_ITERATION Get an eigenvector given a close guess of it
 
 [m, ign] = size(A);
 v_old = v_guess;
 
-for k = 2:101
-  lambda_old = rayleigh_quotient(A, v_old);
-  M = A-lambda_old*eye(m);
-  if abs(det(M)) < 0.000001
-    k
-    break
-  end
+iterations = 0;
+lambda = rayleigh_quotient(A, v_old);
+M = A-lambda*eye(m);
+while abs(det(M)) < 0.000001
+  iterations = iterations + 1;
   w = M\v_old;
   v_old = w/norm(w);
+  lambda = rayleigh_quotient(A, v_old);
+  M = A-lambda*eye(m);
 end
 
