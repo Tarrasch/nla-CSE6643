d22d5d741407ca1597b1619fc59c11d3953de397
Arash Rouhani <miffoljud@gmail.com>
2012-12-07 18:04:32 -0500

    Rename pics too

--- src/2/fig_loop.m
+++ src/2/fig_loop.m
@@ -1,11 +1,14 @@
 desc = ['2' '-' tc_name '-' a_name '-' num2str(lambda)];
-store_fig = @(fn) saveas(gcf, ['fig/' desc '-' fn], 'png');
-hold on
-for n = [100 200 400 800 1600 3200]
-  store = @(c, fn) tofile(desc, c, fn, n);
-  exists = @(fn) exist(makepath(desc, fn, n), 'file');
-  solve_2
+dest_fig = ['fig/' desc '-' fn]
+if ~exist([dest_fig '.png'], 'file')
+  store_fig = @(fn) saveas(gcf, dest_fig, 'png');
+  hold on
+  for n = [100 200 400 800 1600 3200]
+    store = @(c, fn) tofile(desc, c, fn, n);
+    exists = @(fn) exist(makepath(desc, fn, n), 'file');
+    solve_2
+  end
+  hold off
+  store_fig('u')
+  clf
 end
-hold off
-store_fig('u')
-clf
